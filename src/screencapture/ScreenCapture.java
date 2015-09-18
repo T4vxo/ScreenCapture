117d1abfdf7160b8182e4236c4125e249109823e
T4vxo <daniel.lundberg@utb.vxo.se>
2015-09-18 14:40:32 +0200

    added some comment

--- src/screencapture/ScreenCapture.java
+++ src/screencapture/ScreenCapture.java
@@ -15,6 +15,7 @@ public class ScreenCapture {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       //create the form
        ScreenCaptureForm form = new ScreenCaptureForm();
        form.setVisible(true);
     }
