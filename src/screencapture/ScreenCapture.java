1bee4fd257e35573a4f4adaa4a20eb4ca7ba78ea
T4vxo <daniel.lundberg@utb.vxo.se>
2015-09-18 08:49:29 +0200

    initial commit

---
+++ src/screencapture/ScreenCapture.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package screencapture;
+
+/**
+ *
+ * @author Lobo
+ */
+public class ScreenCapture {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ScreenCaptureForm form = new ScreenCaptureForm();
+       form.setVisible(true);
+    }
+    
+}
